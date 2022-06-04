2cdd4a8540db9ba1b9e1129847eebb2e8d25e0ae
ahmedJamaaaal <a.abdelaziz@merge.ws>
2022-06-04 16:17:46 +0200

    fixed postion

--- components/home/mission.vue
+++ components/home/mission.vue
@@ -1,8 +1,9 @@
 <template>
   <div class="mission p-12">
           <div class="number-heading">2</div>
-    <v-row justify="center" align="center">
-      <v-col cols="10">
+      <v-row justify="center" align="center">
+    <div class="mission-card">
+      <v-col cols="8">
         <div class="heading text-center">
         <h1>The Mission</h1>
         </div>
@@ -19,6 +20,7 @@
           </div>
         </div>
       </v-col>
+    </div>
     </v-row>
   </div>
 </template>
@@ -39,20 +41,27 @@ export default {
 
    background-size: 100% 70%;
   background-color: #efefef;
-  margin: 0 auto;
+
 }
 
-.sub-heading {
- max-width: 70%;
-}
-.heading {
+.mission-card {
+
+
+  border: 2px solid #000;
+  width: 100;
+  }
+.mission { position: relative;
+
 
 }
 .number-heading {
+  position: absolute;
   font-size: 250px;
   line-height: 250px;
-  width: 10%;
-float: left;
+  top: 0;
+  left: 0;
+  color: #000;
+  z-index: 11111111;
 }
 
 </style>
